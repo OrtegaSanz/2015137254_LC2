95220d0e1ea535e41be8e8d8bcd58e74964d6fdb
Carlos <carlos-eduardo-1995@live.com>
2017-06-08 14:22:23 -0500

    Controller

---
+++ 2015137254-SLN/2015137254-MVC/Controllers/AdministradorEquipoesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using _2015137254_ENT.Entities;
+using _2015137254_PER;
+
+namespace _2015137254_MVC.Controllers
+{
+    public class AdministradorEquipoesController : Controller
+    {
+        private _2015137254DbContext db = new _2015137254DbContext();
+
+        // GET: AdministradorEquipoes
+        public ActionResult Index()
+        {
+            return View(db.AdministradorEquipos.ToList());
+        }
+
+        // GET: AdministradorEquipoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AdministradorEquipo administradorEquipo = db.AdministradorEquipos.Find(id);
+            if (administradorEquipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(administradorEquipo);
+        }
+
+        // GET: AdministradorEquipoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdministradorEquipoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AdministradorEquipoId")] AdministradorEquipo administradorEquipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AdministradorEquipos.Add(administradorEquipo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(administradorEquipo);
+        }
+
+        // GET: AdministradorEquipoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AdministradorEquipo administradorEquipo = db.AdministradorEquipos.Find(id);
+            if (administradorEquipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(administradorEquipo);
+        }
+
+        // POST: AdministradorEquipoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AdministradorEquipoId")] AdministradorEquipo administradorEquipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(administradorEquipo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(administradorEquipo);
+        }
+
+        // GET: AdministradorEquipoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AdministradorEquipo administradorEquipo = db.AdministradorEquipos.Find(id);
+            if (administradorEquipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(administradorEquipo);
+        }
+
+        // POST: AdministradorEquipoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AdministradorEquipo administradorEquipo = db.AdministradorEquipos.Find(id);
+            db.AdministradorEquipos.Remove(administradorEquipo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
